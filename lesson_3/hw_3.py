adbf2f2da9835641764ca785ebe26a736481ca5d
P1ayMouse <artem@ryzhenko.com>
2022-07-26 13:58:24 +0300

    Final of first task

---
+++ lesson_3/hw_3.py
@@ -0,0 +1,11 @@
+print("Привіт, мене звати Джарвіс! Як тебе звати?\n")
+name = input("\tВкажіть своє імʼя: ")
+print("\nВітаю", name + "!\n")
+
+firstNumber = input("\tЗагадайте перше число: ")
+secondNumber = input("\tЗагадайте друге число: ")
+firstNumber = int(firstNumber)
+secondNumber = int(secondNumber)
+
+summ = firstNumber + secondNumber
+print("\nСума чисел", firstNumber, "та", secondNumber, "рівна", summ)
