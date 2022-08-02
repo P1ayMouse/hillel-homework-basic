27fe1eb4b420d83d345b4e70605081498c96b15c
P1ayMouse <artem@ryzhenko.com>
2022-08-02 22:48:43 +0300

    Retry hw_5.py

--- lesson_5/hw_5.py
+++ lesson_5/hw_5.py
@@ -5,10 +5,11 @@ while input_number > 9:
     unmodul_number = input_number // 10
     modul_number = input_number % 10
     input_number //= 10
-    while unmodul_number != 0:
+    while unmodul_number != 0 and yes != True:
         if modul_number == unmodul_number % 10:
             print("Yes")
             yes = True
+            break
         unmodul_number //= 10
 
 if yes != True:
