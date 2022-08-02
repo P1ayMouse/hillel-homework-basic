6eea5eceae3f8f856a3ca7ed0932fd0e8fc7bb24
P1ayMouse <artem@ryzhenko.com>
2022-08-02 16:25:45 +0300

    final of program hw_5.1.py

--- lesson_5/hw_5.1.py
+++ lesson_5/hw_5.1.py
@@ -3,12 +3,15 @@ n = 0
 
 while n < 1000:
     n += 1
-#    print(n)
     result = n * n
-    while result > 0:
-        modul_result = result % 10
-#        print(modul_result)
-        result //= 10
-#        print(result)
-        if modul_result == n:
-            print(n, "*", n, "=", n * n)
+
+    count = 1
+    i = n
+    while i > 0:
+        i //= 10
+        count *= 10
+
+    modul_result = result % count
+    if modul_result == n:
+        print(n, "*", n, "=", n * n)
+
