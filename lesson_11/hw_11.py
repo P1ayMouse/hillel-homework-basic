ea99b13e829016d1003f9212f649d47907394a28
P1ayMouse <artem@ryzhenko.com>
2022-08-22 22:03:45 +0300

    final of program hw_11.py

---
+++ lesson_11/hw_11.py
@@ -0,0 +1,9 @@
+def list_number(a, b):
+    for i in a:
+        for j in a:
+            if a.index(i) != a.index(j) and i + j == b:
+                return True
+
+print("list_number retutn:", list_number([1, 5, 6, 2, 10, 22], 32))
+print("list_number retutn:", list_number([1, 5, 6, 2, 10, 22], 2))
+
