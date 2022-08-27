a4f1b2e4055f9f03354eaac86a5af5e3238718cd
P1ayMouse <artem@ryzhenko.com>
2022-08-27 22:38:19 +0300

    clean code rework of program hw_11.1.py

--- lesson_11/hw_11.1.py
+++ lesson_11/hw_11.1.py
@@ -1,1 +1,2 @@
-print("Answer 1:", list(map(lambda x, y=2: x ** y, [3, 6, 9])), "\nAnswer 2:", list(map(lambda x, y=2: x ** y, [3, 6, 9], [1, 2, 3])))
+print("Answer 1:", list(map(lambda x, y=2: x ** y, [3, 6, 9])),
+      "\nAnswer 2:", list(map(lambda x, y=2: x ** y, [3, 6, 9], [1, 2, 3])))
