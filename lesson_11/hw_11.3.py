ad83639d75a2cc9959695e33857ce733da21f5e6
P1ayMouse <artem@ryzhenko.com>
2022-08-27 22:12:53 +0300

    rework of program hw_11.3.py

--- lesson_11/hw_11.3.py
+++ lesson_11/hw_11.3.py
@@ -5,9 +5,11 @@ def table(func):
         n.sort()
         print("⎯" * 18)
         for i in n:
-            print("⎢" + f'{str(i):>4}' + "⎪" + f'{"Кратне 3" if i % 3 == 0 else "Не кратне 3":>12}' + "⎪" + f'{"Парне" if i % 2 == 0 else "Не парне":>10}' + "⎥")
+            print("⎢" + f'{str(i):>4}' + "⎪" + f'{"Кратне 3" if i % 3 == 0 else "Не кратне 3":>12}' + "⎪" +
+                  f'{"Парне" if i % 2 == 0 else "Не парне":>10}' + "⎥")
         print("⎯" * 18)
         return n
+
     return wrapper
 
 
@@ -16,5 +18,6 @@ def numbers(number):
     import random
     random_numbers = set(random.randint(1, 100) for i in range(number))
     return random_numbers
+
 
 numbers(10)
