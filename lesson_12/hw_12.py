e71c8e79aee172780b298044037e647537f3816c
P1ayMouse <artem@ryzhenko.com>
2022-08-29 14:42:28 +0300

    rework of program hw_12.py

--- lesson_12/hw_12.py
+++ lesson_12/hw_12.py
@@ -8,14 +8,17 @@ arr = [[random.randint(1, 50) for j in range(N)] for i in range(N)]
 matrix_sum = [sum([arr[j][i] for j in range(N)]) for i in range(N)]
 arr.append(matrix_sum)
 
+
 def enter_matrix(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             print(f'{matrix[i][j]:>{len(str(len(matrix))*2) + 2}}', end="\t")
         print('\n')
 
+
 print("\nДо сортування:")
 enter_matrix(arr)
+
 
 def bubble_sort(matrix):
     for i in range(len(matrix) - 1):
@@ -28,6 +31,7 @@ def bubble_sort(matrix):
                 if matrix[j][i] > matrix[j + 1][i]:
                     matrix[j][i], matrix[j + 1][i] = \
                         matrix[j + 1][i], matrix[j][i]
+
 
 print("\nПісля сортування:")
 
