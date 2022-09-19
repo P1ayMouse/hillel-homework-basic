45bdd74ed904d6aa682e71f0ae22bc082b277544
P1ayMouse <artem@ryzhenko.com>
2022-09-19 13:10:58 +0300

    rework of program hw_18.py

--- Lesson_16/hw_18.py
+++ Lesson_16/hw_18.py
@@ -25,38 +25,6 @@ def boat_check(y, x, boat_location_check):
     return boat_location_check
 
 
-# def bot():
-#     bot_board = Board()
-#     bot_board.draw_board()
-#     boats = set()
-#
-#     b = 0
-#     while b < 4:
-#         location_x = random.randint(0, 9)
-#         location_y = random.randint(0, 9)
-#         if f'[{location_y}, {location_x}]' not in boats:
-#             bot_board.board_selections[location_y][location_x] = '⬜'
-#             boats = boat_check(location_y, location_x, boats)
-#             b += 1
-#
-#     b = 0
-#     while b < 1:
-#         location_x = random.randint(0, 9)
-#         location_y = random.randint(0, 6)
-#         bool_check = True
-#
-#         for i in range(4):
-#             if f'[{location_y + i}, {location_x}]' in boats:
-#                 bool_check = False
-#
-#         if bool_check == True:
-#             for j in range(4):
-#                 bot_board.board_selections[location_y + j][location_x] = '⬜'
-#                 boats = boat_check(location_y + j, location_x, boats)
-#                 b += 1
-#     bot_board.draw_board()
-
-
 def right_locate(board, boat_size):
     bool_check = True
 
